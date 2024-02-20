c792a859f1067f4f1ab66327b1778585c9be78f3
moonch030 <cogus2010@naver.com>
2024-02-20 13:37:23 +0900

    2024-02-20 @Transactional

--- src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -9,21 +9,25 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @SpringBootTest
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
 
-    @Autowired
+/*    @Autowired
     PlatformTransactionManager transactionManager;
     TransactionStatus status;
 
@@ -31,7 +35,7 @@ class ItemRepositoryTest {
     void beforeEach(){ //testcase 실행되기 전에 실행됨
         //트랜잭션 시작
         status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-    }
+    }*/
 
     @AfterEach
     void afterEach() {
@@ -40,9 +44,12 @@ class ItemRepositoryTest {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
         //트랜잭션 롤백
-        transactionManager.rollback(status);
+//        transactionManager.rollback(status);
     }
 
+//    @Commit // 롤백이 기본이지만 db에 저장이 잘 되어있는지 확인하고 싶다면
+//    @Rollback(false) //= 커밋과 동일
+//    @Transactional
     @Test
     void save() {
         //given
