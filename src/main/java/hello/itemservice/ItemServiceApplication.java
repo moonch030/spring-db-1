926b270381f63740f48472e346bd18aa2e01bcd5
moon <cogus2010@naver.com>
2024-02-26 20:21:33 +0900

    2024-02-26 Querydsl 적용

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -15,7 +15,7 @@ import javax.sql.DataSource;
 
 
 @Slf4j
-@Import(SpringDataJpaConfig.class)
+@Import(QuerydslConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
