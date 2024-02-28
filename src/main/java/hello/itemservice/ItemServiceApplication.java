b27ff0dc779ad520cf6f83c4b72637e83016bcdb
moon <cogus2010@naver.com>
2024-02-28 17:48:47 +0900

    2024-02-28 실용적인 구조

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -15,7 +15,7 @@ import javax.sql.DataSource;
 
 
 @Slf4j
-@Import(QuerydslConfig.class)
+@Import(V2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
